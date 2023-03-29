6b762ac03e58d257ed2471e20cf4f030803a7efb
nguyenhung10012003 <nguyenhung1012003@gmail.com>
2023-03-29 12:45:38 +0700

    fix token exprised

---
+++ src/Server/spotifyRequest.js
@@ -0,0 +1,31 @@
+const SpotifyWebApi = require('spotify-web-api-node');
+require('dotenv').config({path: __dirname + '../../../.env'});
+
+const spotifyApi = new SpotifyWebApi({
+                                         clientId: process.env.CLIENT_ID,
+                                         clientSecret: process.env.CLIENT_SECRET,
+                                     });
+
+    if (!spotifyApi.getAccessToken()) {
+        spotifyApi.clientCredentialsGrant()
+            .then((data) => {
+                spotifyApi.setAccessToken(data.body['access_token']);
+                console.log(data.body['access_token']);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    setInterval(() => {
+        spotifyApi.clientCredentialsGrant()
+            .then((data) => {
+                spotifyApi.setAccessToken(data.body['access_token']);
+                console.log('Refresh access token: ' + data.body['access_token']);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }, 3599 * 1000)
+
+module.exports = spotifyApi;
