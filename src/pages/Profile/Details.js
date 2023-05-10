56db179695ff6f678fb713c9e770cf107b8e9dc7
kiu-chan <baokhanhntby@gmail.com>
2023-05-10 20:39:28 +0700

    checkpass

--- src/pages/Profile/Details.js
+++ src/pages/Profile/Details.js
@@ -8,31 +8,42 @@ function Detail(props) {
     const navigate = useNavigate();
     const {getUser} = useAuth();
     const [dataProfile, setDataProfile] = useState([]);
+    const [confirmPass, setconfirmPass] = useState();
+    const [checkPass, setCheckPass] = useState(false);
+    const [isDivVisible, setIsDivVisible] = useState(false);
+
 
     useEffect(() => {
         const fetchLibrary = async () => {
             try {
                 const response = await profileApi.getDataUser(getUser().email);
                 setDataProfile(response);
+                setconfirmPass(response.password);
             } catch(err) {
                 console.log(err);
             }
         }
 
         fetchLibrary();
-    }, [dataProfile])
+    },[])
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
+
+        if(checkPass) {
         profileApi.editProfile(dataProfile._id, dataProfile);
 
         props.onClick(false);
-
+        
         navigate(`/profile`);
+        }
     }
 
     const handleChange = (event) => {
+        if (event.target.name == 'confirmPassword')
+            setconfirmPass(event.target.value);
+        else
         setDataProfile({
           ...dataProfile,
           [event.target.name]: event.target.value,
@@ -42,6 +53,13 @@ function Detail(props) {
   
     const handleClick = () => {
         props.onClick(false);
+    };
+
+    const checkPassword = () => {
+        if(dataProfile.password == confirmPass && dataProfile.password.length > 6)
+            setCheckPass(!checkPass);
+        else if(!isDivVisible)
+            setIsDivVisible(!isDivVisible);
     };
 
     return (
@@ -59,7 +77,12 @@ function Detail(props) {
                         <input type={'text'} value={dataProfile.name} onChange={handleChange} name='name' className={styles['list-hidden_content-input']}></input>
                         <label className={styles['list-hidden_content-label']}>Mật khẩu</label>
                         <input type={'text'} value={dataProfile.password} onChange={handleChange} name='password' className={styles['list-hidden_content-input']}></input>
-                        <button type="submit" className={styles['list-hidden_content-button']}>Lưu</button>
+                        <label className={styles['list-hidden_content-label']}>Xác nhận mật khẩu</label>
+                        <input type={'text'} value={confirmPass} onChange={handleChange} name='confirmPassword' className={styles['list-hidden_content-input']}></input>
+                        {isDivVisible && (
+                            <p>sai mk</p>
+                        )}
+                        <button type="submit" onClick={checkPassword} className={styles['list-hidden_content-button']}>Lưu</button>
                     </form>
                 </div>
             </div>
